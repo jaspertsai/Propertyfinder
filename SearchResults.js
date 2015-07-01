c364db6068cf004bbd72feb583b564e87309fde8
jaspertsai <jaspert99@gmail.com>
2015-06-30 18:27:00 -0700

    successfully add image

--- SearchResults.js
+++ SearchResults.js
@@ -11,8 +11,40 @@ var {
   Component
 } = React;
 
+
+
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
 class SearchResults extends Component {
  
+ rowPressed(propertyGuid) {
+  var property = this.props.listings.filter(prop => prop.guid === propertyGuid)[0];
+}
   constructor(props) {
     super(props);
     var dataSource = new ListView.DataSource(
@@ -23,15 +55,25 @@ class SearchResults extends Component {
   }
  
   renderRow(rowData, sectionID, rowID) {
-    return (
-      <TouchableHighlight
-          underlayColor='#dddddd'>
-        <View>
-          <Text>{rowData.title}</Text>
+  var price = rowData.price_formatted.split(' ')[0];
+ 
+  return (
+    <TouchableHighlight onPress={() => this.rowPressed(rowData.guid)}
+        underlayColor='#dddddd'>
+      <View>
+        <View style={styles.rowContainer}>
+          <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+          <View  style={styles.textContainer}>
+            <Text style={styles.price}>£{price}</Text>
+            <Text style={styles.title} 
+                  numberOfLines={1}>{rowData.title}</Text>
+          </View>
         </View>
-      </TouchableHighlight>
-    );
-  }
+        <View style={styles.separator}/>
+      </View>
+    </TouchableHighlight>
+  );
+}
  
   render() {
     return (
